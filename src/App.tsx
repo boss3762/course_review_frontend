a3d4ae738ebe7597a503574b693bbd2622877d2d
Phattarapol <wpphattarapol@gmail.com>
2026-02-16 14:50:39 +0700

    feat: Fetch and display a list of courses from the API instead of a single message.

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,23 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
-  const [message, setMessage] = useState('My message');
+  const [courses, setCourses] = useState<any[]>([]);
 
   useEffect(() => {
     fetch('http://localhost:3000/courses')
     .then(res => res.json())
-    .then(obj => {
-      setMessage(obj.message);
+    .then(courses => {
+      setCourses(courses);
     });
   },[]);
 
   return (
     <div className="App">
-      {message}
+      <ul>
+        {courses.map(item => (
+          <li key={item.id}>{item.number} - {item.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
